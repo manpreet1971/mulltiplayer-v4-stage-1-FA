7ba6431b616c2f4e6957ffa69073c76a7a3deed3
manpreet1971 <73438967+manpreet1971@users.noreply.github.com>
2023-06-06 18:13:05 +0530

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -6,19 +6,11 @@ class Player {
     this.positionY = 0;
   }
 
-  addPlayer() {
-    var playerIndex = "players/player" + this.index;
 
-    if (this.index === 1) {
-      this.positionX = width / 2 - 100;
-    } else {
-      this.positionX = width / 2 + 100;
-    }
 
-    database.ref(playerIndex).set({
-      name: this.name,
-      positionX: this.positionX,
-      positionY: this.positionY,
-    });
-  } */
+
+
+
+
+
 }
